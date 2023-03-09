877790e554e025408acf15b6492251ad9f5355c9
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-03-09 18:55:22 +0900

    module example

---
+++ ch01/ex1-04.ts
@@ -0,0 +1,9 @@
+/*
+import * as fs from 'fs'
+
+export function writeFile(filepath: string, content: any) {
+    fs.writeFile(filepath, content, (err) => {
+        err && console.log('error', err)
+    })
+}
+*/
