3b02dd752c321386f1178fd2c2698f42b4af8f99
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-03-09 21:19:23 +0900

    promise & async/await example

---
+++ ch01/ex1-06.ts
@@ -0,0 +1,11 @@
+/*
+async function get() {
+    let values = []
+    values.push(await Promise.resolve(1))
+    values.push(await Promise.resolve(2))
+    values.push(await Promise.resolve(3))
+    return values
+}
+
+get().then(value => console.log(value))
+*/
