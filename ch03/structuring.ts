ca4224723777367de59020a88b28912ec1abe616
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-03-16 01:14:57 +0900

    structuring example

---
+++ ch03/structuring.ts
@@ -0,0 +1,7 @@
+import {IPerson, ICompany} from "./IPerson_ICompany";
+
+let jack: IPerson = {name: 'Jack', age: 25},
+    jane: IPerson = {name: 'Jane', age: 25};
+
+let apple: ICompany = {name: 'Apple Computer, Inc', age: 43},
+    ms: ICompany = {name: 'Microsoft', age: 44};
