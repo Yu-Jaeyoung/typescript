3e42380d32583ede4412ee7e596f91e484fac3e9
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-03-16 01:06:47 +0900

    extends class example

--- ch03/Person5.ts
+++ ch03/Person5.ts
@@ -2,3 +2,11 @@ abstract class AbstractPerson5 {
     abstract name: string;
     constructor(public age?: number) {}
 }
+
+class Person5 extends AbstractPerson5 {
+    constructor(public name: string, age?: number) {
+        super(age);
+    }
+}
+let jack5 : Person5 = new Person5('Jack',25);
+console.log(jack5) // Person5 { name: 'Jack', age: 25 }
