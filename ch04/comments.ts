cee54fe6bed83c0ae1f0cc4567f623f4dba10130
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-03-28 13:30:36 +0900

    function declaration example

---
+++ ch04/comments.ts
@@ -0,0 +1,7 @@
+/*
+function printMe(name: string, age: number): void {
+    console.log(`name: ${name}, age: ${age}`)
+}
+*/
+
+let printMe: (string, number) => void = function (name: string, age: number): void {}
