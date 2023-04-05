a51bde8fba31cdb351b8b2d86e49c17903294f0d
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-04-06 01:30:09 +0900

    pure function & array

---
+++ ch05/mergeArray.ts
@@ -0,0 +1,9 @@
+export const mergeArray = <T>(...arrays: readonly T[][]): T[] => {
+    let result: T[] = [];
+    for (let index = 0; index < arrays.length; index++) {
+        const array: T[] = arrays[index];
+        // result와 array 배열을 각각 전개(spread)하고 결합(merge)해야 T[] 타입 배열을 생성 가능
+        result = [...result, ...array];
+    }
+    return result;
+}
